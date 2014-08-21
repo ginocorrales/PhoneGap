4e1c1886abc8630fd4bda7a8fbc3e08fb9375de3
Gino Corrales <ginocodel@gmail.com>
2014-08-21 11:12:05 -0500

    few more things
    now is not working the autosearch

--- js/main.js
+++ js/main.js
@@ -21,6 +21,23 @@ var app = {
     
     }
 
+    showAlert: function (messag , title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
+    }
+
+    initialize: function() {
+    var self = this;
+    this.store = new MemoryStore(function() {
+        self.showAlert('Store Initialized', 'Info');
+    });
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+
+}
+},
+
 };
 
 app.initialize();
