6097794ff390fcf38916a74cf6e48474ddfff3e5
Gino Corrales <ginocodel@gmail.com>
2014-08-23 21:18:07 -0500

    up to step 5
    create two templates to streamline the code of the Employee Directory
    application. We use Handlebar.js but the same result can be achieved
    using the other HTML template solutions.

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ var app = {
         }
     },
 
+    /* //Step 3
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
@@ -20,13 +21,46 @@ var app = {
             }
         });
     },
+    */
+    
+    // step 5
+    findByName: function() {
+        var self = this;
+        this.store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
+        });
+    },
+
+    /* //Step 3 
+    renderHomeView: function() {
+        var html =
+                "<div class='header'><h1>Ginos Employee Search</h1></div>" +
+                "<div class='search-view'>" +
+                "<input class='search-key'/>" +
+                "<ul class='employee-list'></ul>" +
+                "</div>"
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+    */
+    
+    // step 5
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
 
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized - This is a GinosAlert', 'Info');
+            //self.showAlert('Store Initialized - This is a GinosAlert', 'Info');
+            self.renderHomeView();
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        //$('.search-key').on('keyup', $.proxy(this.findByName, this));
+        
+        // to compile the HTML template to render the Home View and the Employee list item...
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     }
 
 };
