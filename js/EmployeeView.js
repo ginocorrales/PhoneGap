f70c14ecd6355d468887b2cff861b6b93641605f
Gino Corrales <ginocodel@gmail.com>
2014-08-29 18:17:50 -0500

    Part 11 - Using the Contacts API
    Using the Contacts API - add an employee to the device’s contact list.
    1. In index.html, add the following list item to the employee template
    2. In the initialize() function of EmployeeView, register an event
    listener for the click event of the Add to Contacts list item:
    3. n EmployeeView, define the addToContacts event handler

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -10,6 +10,7 @@ var EmployeeView = function(employee) {
     this.initialize = function() {
         this.el = $('<div/>');
         this.el.on('click', '.add-location-btn', this.addLocation);
+        this.el.on('click', '.add-contact-btn', this.addToContacts);
     };
  
     // Define a render() function 
@@ -30,6 +31,24 @@ var EmployeeView = function(employee) {
 	        function() {
 	            alert('Error getting location');
 	        });
+	    return false;
+	};
+
+	//Part 11 - Using the Contacts API - add an employee to the device’s contact list.
+	this.addToContacts = function(event) {
+	    event.preventDefault();
+	    console.log('addToContacts');
+	    if (!navigator.contacts) {
+	        app.showAlert("Contacts API not supported", "Error");
+	        return;
+	    }
+	    var contact = navigator.contacts.create();
+	    contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+	    var phoneNumbers = [];
+	    phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+	    phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
+	    contact.phoneNumbers = phoneNumbers;
+	    contact.save();
 	    return false;
 	};
 
